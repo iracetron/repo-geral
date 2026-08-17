bf738a69504f8b9be4804aae28cbcef1048c5ed7
iracetron <iracet.ron@gmail.com>
2026-08-17 16:46:12 -0300

    feat: adiciona pasta relacionada a JavaScript

---
+++ JavaScript/variaveis/exec.js
@@ -0,0 +1,35 @@
+// Declarar uma variável com o seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+// Atribuir valor a sua comida favorita
+
+// Declarar 5 variáveis diferentes sem valores
+
+const name = 'Ronaldo';
+
+let age = 26
+
+var comidaFavorita;
+comidaFavorita = 'lasanha';
+
+var nickname,
+    address,
+    height;
+
+let weight,
+    yearBirth;
+
+console.log(name, age, comidaFavorita)
+console.log(nickname, address, height, weight, yearBirth)
+
+var nome = 'Ronaldo';
+var sobrenome = 'Iracet';
+
+console.log(nome + ' ' + sobrenome)
+
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto)
---
+++ JavaScript/variaveis/script.js
@@ -0,0 +1,18 @@
+var nome = 'Ronaldo';
+var idade = 25;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal)
+
+var sobrenome = 'Iracet', cidade = 'Porto Alegre', emFormacao = true;
+
+console.log(sobrenome, cidade, emFormacao)
+
+var idade = 26;
+console.log(idade)
